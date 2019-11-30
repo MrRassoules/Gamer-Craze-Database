631f05a1ed25adac99a3297657a7ec4bdace15ec
Christopher Stannard <Cstannard@ymail.com>
2019-11-30 14:51:54 -0500

    Added a base home

--- GamerCrazeProject/inventory/views.py
+++ GamerCrazeProject/inventory/views.py
@@ -1,1 +1,16 @@
 from django.shortcuts import render
+from inventory.models import MTGSingle
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_cards = MTGSingle.objects.all().count()
+
+
+    context = {
+        'num_cards': num_cards,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
