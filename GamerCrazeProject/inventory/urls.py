0be52eb088d1166b70ce24a56083f7e9e971b575
Christopher Stannard <Cstannard@ymail.com>
2019-12-01 19:00:17 -0500

    image fixed

--- GamerCrazeProject/inventory/urls.py
+++ GamerCrazeProject/inventory/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('mtgcards/', views.MTGCardListView.as_view(), name='mtgcards'),
     path('single/<str:pk>', views.MTGCardDetailView.as_view(), name='mtgcard-detail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
